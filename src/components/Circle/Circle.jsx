94f17cb1ff52e852326e01c20b4a3c9a97a6da2b
kluchkos <semenkluchko@gmail.com>
2021-10-13 03:57:44 +0300

    finished tasks

---
+++ src/components/Circle/Circle.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './Circle.css';
+
+
+function Circle() {
+
+    const [x, isX] = useState(0);
+    const [y, isY] = useState(0);
+
+    const handleMouseMove = (e) => {
+        isX(e.pageX / window.innerWidth);
+        isY(e.pageY / window.innerWidth);
+    };
+
+    return (
+        <section className="section-circle" onMouseMove={handleMouseMove}>
+            <div className="circle"></div>
+            <span className="circle-text" style={{
+                transform: `translate(${x * 25}px, ${y * 25}px)`
+            }} >full-cycle event agency</span>
+
+        </section>
+    );
+}
+
+export default Circle;
---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './Page.css';
+import Marquee from 'react-double-marquee';
+import Dropright from '../Dropright/Dropright';
+import Circle from '../Circle/Circle';
+import Showreel from '../Showreel/Showreel';
+
+
+
+function Page() {
+    const [scroll, isScrolling] = useState(false);
+    const handleWheel = (e) => {
+        isScrolling(e.pageX / 250 % Math.PI);
+        console.log(scroll);
+    };
+    return (
+        <div className="wrapper" onWheel={handleWheel}>
+            <div className="marquee-one">
+                <Marquee scrollWhen="always">FULL-CYCLE EVENT AGENCY FULL-CYCLE EVENT AGENCY</Marquee>
+            </div>
+            <div className="marquee-two">
+                <Marquee scrollWhen="always">FULL-CYCLE EVENT AGENCY FULL-CYCLE EVENT AGENCY</Marquee>
+            </div>
+            <header className="header">
+                <nav className="nav">
+                    <Dropright />
+                    <div className="logo">
+                        <span className="logo-text">logo</span>
+                    </div>
+                </nav>
+            </header>
+            <main className="main">
+                <Circle />
+                <Showreel wheel={scroll} />
+            </main>
+            <aside className="aside-one">
+                <span className="aside-text-one">where?</span>
+            </aside>
+            <aside className="aside-two">
+                <span className="aside-text-two">what?</span>
+            </aside>
+            <footer className="footer">
+                <span className="footer-text">who?</span>
+            </footer>
+        </div >
+    );
+}
+
+export default Page;
